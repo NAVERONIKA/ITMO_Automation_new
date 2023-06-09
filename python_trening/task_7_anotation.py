cfb9f6d3b401201139accf919668107f78650eb5
veronikanazarenko <veronika.nazarenko@gmail.com>
2023-06-09 18:19:43 +0300

    init repo

---
+++ python_trening/task_7_anotation.py
@@ -0,0 +1,24 @@
+a: int=5
+b: str = 'строка'
+с: list = [1, 2]
+
+
+def indent(s: str, width: int) -> str:
+    return ' ' * (max(0, width - len(s))) + s
+
+print(indent('1.2.3' , 123))
+
+
+
+def a(s: str='') -> int:
+    return len(s)
+
+
+def min_list(a: list, b: list) -> int:
+    return max(len(a), len(b))
+
+def b(l=[1, 2,3]):
+   print( append_list(['один'б ])
+    return l
+
+
